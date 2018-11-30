3ab7346bbc29fb0e60657cd315742b0ae22f9367
Céline Bensoussan <celine.bensoussan@gmail.com>
2018-11-30 12:24:46 -0500

    Add 2 import rules in base

--- modules/base/rules/imports.js
+++ modules/base/rules/imports.js
@@ -16,6 +16,8 @@ module.exports = {
     'import/no-nodejs-modules': 'off',
     'import/no-named-as-default': 'error',
     'import/prefer-default-export': 'off',
+    'import/group-exports': 'off',
+    'import/no-default-export': 'off',
     'import/order': ['error', { groups: ['builtin', 'external', 'internal'] }],
 
     // Disabled due to https://github.com/tleunen/eslint-import-resolver-babel-module/issues/46
