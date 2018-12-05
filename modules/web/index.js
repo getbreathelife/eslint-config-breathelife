3ee25a62e69195b55529959971e68d386eecb0dd
Janic Beauchemin <janic@breathelife.com>
2018-12-05 10:36:57 -0500

    Remove babel-module import resolver

--- modules/web/index.js
+++ modules/web/index.js
@@ -4,7 +4,6 @@ module.exports = {
     'eslint-config-breathelife-base',
 
     './rules/env',
-    './rules.imports',
     './rules/react',
   ].map(require.resolve),
 };
--- modules/web/rules/imports.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  settings: {
-    'import/resolver': {
-      'babel-module': {},
-    },
-  },
-};
